128874abaf38ccdc50b70c0323032d8db7111150
Zachariasz Strus <zachariasz.strus@gmail.com>
2022-12-06 10:16:18 +0100

    code format

--- frontend/src/ui/MediaBlock.tsx
+++ frontend/src/ui/MediaBlock.tsx
@@ -4,13 +4,13 @@ import { ComponentSharedMedia } from "api";
 interface MediaBlockProps extends Pick<ComponentSharedMedia, "file"> {}
 
 const MediaBlock: FC<MediaBlockProps> = ({ file }) => {
-    return (
-        <img
-            src={file.data?.attributes?.url}
-            alt={file.data?.attributes?.name}
-            className={"max-w-full"}
-        />
-    );
+  return (
+    <img
+      src={file.data?.attributes?.url}
+      alt={file.data?.attributes?.name}
+      className={"max-w-full"}
+    />
+  );
 };
 
 export default MediaBlock;
