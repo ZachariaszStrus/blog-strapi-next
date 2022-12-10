50814feda2608e8e726044ed4e5dc2b0c4f0132a
smlltt <samuel.liotta@gmail.com>
2022-12-10 19:41:47 +0100

    list article hover effect, link to homepage (#25)

--- frontend/src/ui/BlogTitle.tsx
+++ frontend/src/ui/BlogTitle.tsx
@@ -1,19 +1,29 @@
 import { Header } from "@api";
 import React from "react";
 import { Text } from "./Text";
+import { useRouter } from "next/router";
+import NextLink from "next/link";
+import { routes } from "@utils";
 
 interface BlogTitleProps {
   header: Header;
 }
 
 const BlogTitle = ({ header }: BlogTitleProps) => {
+  const { route } = useRouter();
+
   return (
-    <div className="space-y-2">
+    <NextLink
+      className={`space-y-2 ${
+        route === routes.home() ? "pointer-events-none" : "pointer-events-auto"
+      }`}
+      href={routes.home()}
+    >
       <Text h1>{header.title}</Text>
       <Text h3 faded>
         {header.subtitle}
       </Text>
-    </div>
+    </NextLink>
   );
 };
 
